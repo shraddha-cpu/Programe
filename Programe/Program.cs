ed666b90abc9d5ef0d11f47cf9a7321544c10399
shraddha-cpu <shraddhapaygude5337@gmail.com>
2022-09-27 17:13:55 +0530

    Add project files.

---
+++ Programe/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programe
+{
+    public class Student
+    {
+        public string StudName { get; set; }
+        public string City { get; set; }
+    }
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Fees { get; set; }
+        public Student[] Students { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] stud1 = new Student[]
+           {
+                new Student { StudName = "Rahul", City = "Pune" },
+               new Student { StudName = "Rohan", City = "Mumbai" }
+           };
+            Student[] stud2 = new Student[]
+            {
+                new Student { StudName = "Rahul", City = "Pune" },
+               new Student { StudName = "Rohan", City = "Mumbai" },
+               new Student { StudName = "Kishor", City = "Nagpur" }
+            };
+            Student[] stud3 = new Student[]
+           {
+                new Student { StudName = "Dipali", City = "Pune" },
+               new Student { StudName = "Minal", City = "Mumbai" },
+               new Student { StudName = "Ashwini", City = "Nagpur" }
+           };
+
+            Course[] courses = new Course[]
+            {
+                new Course{Id=1,Name="C#",Fees=20000,Students=stud1 },
+                new Course{Id=2,Name="Angular",Fees=20000,Students=stud2 },
+                new Course{Id=3,Name="MVC",Fees=20000,Students=stud3 }
+            };
+
+            foreach (Course c in courses)
+            {
+                Console.WriteLine($"{c.Id} {c.Name}  {c.Fees}");
+                foreach (Student s in c.Students)
+                {
+                    Console.WriteLine($"\t {s.StudName} -> {s.City}");
+                }
+            }
+
+        }
+    }
+}
---
+++ Programe/Programe2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programe
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public float Salary { get; set; }
+    }
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Employee[] Employees { get; set; }
+    }
+
+    internal class Programe2
+    {
+        static void Main(string[] args)
+        {
+           Employee[] emp1 = new Employee[]
+          {
+                new Employee {Id=1 ,Name = "Rahul", City = "Pune" ,Salary=67000},
+               new Employee {Id=2, Name = "Rohan", City = "Mumbai" ,Salary=70000}
+          };
+            Employee[] emp2 = new Employee[]
+            {
+                new Employee {Id=1, Name = "Rahul", City = "Pune",Salary=50000 },
+               new Employee {Id=2, Name = "Rohan", City = "Mumbai" ,Salary=65000},
+               new Employee {Id=3, Name = "Kishor", City = "Nagpur",Salary=71000 }
+            };
+            Employee[] emp3 = new Employee[]
+           {
+                new Employee {Id=1, Name = "Dipali", City = "Pune",Salary=45000 },
+               new Employee {Id=2 ,Name = "Minal", City = "Mumbai", Salary=55000},
+               new Employee {Id=3, Name = "Ashwini", City = "Nagpur",Salary=56000 }
+           };
+
+           Department[] departments = new Department[]
+            {
+                new Department{Id=1,Name="C#",Employees=emp1 },
+                new Department{Id=2,Name="Angular",Employees=emp2 },
+                new Department{Id=3,Name="MVC",Employees=emp3 }
+            };
+
+            foreach (Department d in departments)
+            {
+                Console.WriteLine($"{d.Id} {d.Name} ");
+                foreach (Employee e in d.Employees)
+                {
+                    Console.WriteLine($"\t {e.Id} ->{e.Name} -> {e.City} ->{e.Salary}");
+                }
+            }
+
+        }
+    }
+}
+   
