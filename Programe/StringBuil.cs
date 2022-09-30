1a879e2b30fad3fab6c2df2c4e5e83100cfeebfa
shraddha-cpu <shraddhapaygude5337@gmail.com>
2022-09-30 15:25:33 +0530

    stringbuild

--- Programe/StringBuil.cs
+++ Programe/StringBuil.cs
@@ -34,25 +34,23 @@ namespace Programe
 
             /*
               StringBuilder stringBuilder = new StringBuilder("Hello to all");
-            // Append  , AppendFormat  , Insert  , Remove , Replace
-            float salary = 25000.00f;
+            Console.WriteLine("------string builder class----------");
             Console.WriteLine(stringBuilder);
-
+            Console.WriteLine(stringBuilder.GetHashCode());
             stringBuilder.Append(" My name is Rohan");
             Console.WriteLine(stringBuilder);
+            Console.WriteLine(stringBuilder.GetHashCode());
+            Console.WriteLine("------string class ---------");
+            string s1 = "Hello to all";
+            Console.WriteLine(s1);
+            Console.WriteLine(s1.GetHashCode()) ;
 
-            stringBuilder.AppendFormat(" my salary is {0:C}",salary);
-            Console.WriteLine(stringBuilder);
+            s1 = s1 + " This is sample code";
+            Console.WriteLine(s1);
+            Console.WriteLine(s1.GetHashCode());
 
-            stringBuilder.Insert(10, " HI ");
-            Console.WriteLine(stringBuilder);
 
-            stringBuilder.Replace("Rohan", "Suraj");
-
-            Console.WriteLine(stringBuilder);
-
-            stringBuilder.Remove(10, 5);
-            Console.WriteLine(stringBuilder);
+            
         */
         }
     }
