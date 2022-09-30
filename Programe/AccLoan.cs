dad90dbd5b238d95afbd3828c38dd1074e3a8700
shraddha-cpu <shraddhapaygude5337@gmail.com>
2022-09-30 15:22:38 +0530

    collections

---
+++ Programe/AccLoan.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programe
+{
+        public class Loan
+        {
+            public string Type { get; set; }
+            public double RateOfInt { get; set; }
+        }
+        public class Customer
+        {
+            public string Name { get; set; }
+            public int Id { get; set; }
+            public List<Loan> Loans = new List<Loan>();
+        }
+        public class AccountType
+        {
+            public string Type { get; set; }
+            public List<Customer> custmerList = new List<Customer>();
+        }
+        internal class AccLoan
+        {
+      static void Main(string[] args)
+        {
+            List<AccountType> accountList = new List<AccountType>();
+            {
+                new AccountType
+                {
+                    Type="saving",
+                    custmerList =new List<Customer>()
+                    {
+                        new Customer()
+                        {
+                            Id=1,
+                            Name="radhika",
+                            Loans =new List<Loan>()
+                            {
+                                new Loan()
+                                {
+                                    Type= "car loan",
+                                    RateOfInt =8.8,
+                                },
+                                new Loan()
+                                {
+                                    Type ="home loan",
+                                    RateOfInt  =7.7,
+                                }
+                            }
+                        },
+                        new Customer()
+                        {
+                            Id=2,
+                            Name="kavita",
+                            Loans =new List<Loan>()
+                            {
+                                new Loan()
+                                {
+                                    Type= "home loan",
+                                    RateOfInt =6.8,
+                                }
+                            }
+                        },
+                        new Customer()
+                        {
+                            Id=3,
+                            Name="savita",
+                            Loans =new List<Loan>()
+                            {
+                                new Loan()
+                                {
+                                    Type= "education loan",
+                                    RateOfInt =9.8,
+                                }
+                            }
+                        },
+                        new Customer()
+                        {
+                            Id=4,
+                            Name="shubham",
+                            Loans =new List<Loan>()
+                            {
+                                new Loan()
+                                {
+                                    Type= "bike loan",
+                                    RateOfInt =10,
+                                }
+                            }
+                        }
+                    }
+               };
+       
+    
+             foreach (AccountType a in accountList)
+                {
+                    Console.WriteLine(a.Type);
+                    foreach (Customer c in a.custmerList)
+                    {
+                        Console.WriteLine($" \t name={c.Name}-->id={c.Id}");
+                        foreach (Loan l in c.Loans)
+                        {
+                            Console.WriteLine($"\t TypeofLoan= {l.Type}-->RateofLoan={l.RateOfInt}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Programe/StringBuil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programe
+{
+    internal class StringBuil
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder("Hello to all");
+            // Append  , AppendFormat  , Insert  , Remove , Replace
+            float salary = 25000.00f;
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Append(" My name is Rohan");
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.AppendFormat(" my salary is {0:C}", salary);
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Insert(10, " HI ");
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Replace("Rohan", "Suraj");
+
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Remove(10, 5);
+            Console.WriteLine(stringBuilder);
+
+        }
+    }
+}
