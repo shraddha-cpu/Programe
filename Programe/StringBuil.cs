8eb67ce1065930fba8b0ebffb0f5c2f69c8ed735
shraddha-cpu <shraddhapaygude5337@gmail.com>
2022-09-30 15:24:30 +0530

    stringbuild

--- Programe/StringBuil.cs
+++ Programe/StringBuil.cs
@@ -31,6 +31,29 @@ namespace Programe
             stringBuilder.Remove(10, 5);
             Console.WriteLine(stringBuilder);
 
+
+            /*
+              StringBuilder stringBuilder = new StringBuilder("Hello to all");
+            // Append  , AppendFormat  , Insert  , Remove , Replace
+            float salary = 25000.00f;
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Append(" My name is Rohan");
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.AppendFormat(" my salary is {0:C}",salary);
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Insert(10, " HI ");
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Replace("Rohan", "Suraj");
+
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Remove(10, 5);
+            Console.WriteLine(stringBuilder);
+        */
         }
     }
 }
